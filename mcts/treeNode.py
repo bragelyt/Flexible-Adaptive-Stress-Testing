63d6a39ab4bbd1cdaf7c234cfa219b3f1bf21078
bragelyt <bragelyt@gmail.com>
2022-01-28 14:41:56 +0100

    Clean up restructure

--- mcts/treeNode.py
+++ mcts/treeNode.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import math, json
 from typing import Dict
 
@@ -7,13 +8,11 @@ class TreeNode:
         self.action = action
         self.parrent = parent
         self.isTerminal = False
-        # self.cumD = None  # REVIEW: When can cumulative distance be passed in? Nodes are allways built after parrents. Might be possible to pass in at first backprop, but messy.
-        
         # ---------- Stats --------- #
         self.timesVisited = 0  # Initiates only after visit so should be one
         self.evaluation = 0
         if self.action == None:
-            self.transProb = 1
+            self.stepReward = 0
         else:
             self.evaluation = 0
 
@@ -24,15 +23,15 @@ class TreeNode:
             params = json.load(f)  # Pass out to main?
         self.explorationCoefficient = params["exploration_coefficient"]
     
-    def visit_node(self):
+    def visitNode(self):
         self.timesVisited += 1
     
-    def add_child(self, newBornAction):
+    def addChild(self, newBornAction) -> TreeNode:
         if newBornAction not in self.children.keys():  # REVIEW: Sloppy fix for randomness in seed generation. If exact same seed generated before, a child is not added.
             self.children[newBornAction] = TreeNode(newBornAction, self)  # NOTE This is the backbone of the tree structure
         return self.children[newBornAction]
     
-    def UCTselect(self):  # Tree policy. "Bandit based Monte-Carlo Planning", Kocsis and Szepervari (2006)
+    def uctSelect(self):  # Tree policy. "Bandit based Monte-Carlo Planning", Kocsis and Szepervari (2006)
         # NOTE: This is left without a check for if children are empty. Should resolve itself through prog widening but might fuck up
         maxValue = -math.inf
         bestChild : TreeNode = None
@@ -45,20 +44,3 @@ class TreeNode:
 
     def eveluate(self, reward): # Called dureing backprop
         self.evaluation = self.evaluation + (reward - self.evaluation)/self.timesVisited
-
-    # def visit_child(self, childNodeState):
-    #     self.childrenVisits[tuple(childNodeState)] += 1
-    
-    # def evaluate_child(self, childNodeState, evaluation):
-    #     childTuple = tuple(childNodeState)
-    #     self.childrenEvaluations[childTuple] = self.childrenEvaluations[childTuple] + (evaluation - self.childrenEvaluations[childTuple])/self.childrenVisits[childTuple]
-
-    # def backpropegate(self, reward, targetNode):
-    #     if self == targetNode:
-    #         pass
-    #     else:  # REVIEW Unsusre if this is correct. Must find out how policy works
-    #         # ! Think this is a mistake, as the step reward is not taken into account as totalReward = reward + self.simulate()
-    #         self.totalEvaluation += reward
-    #         self.timesVisited += 1
-    #         if self.parrent is not None:
-    #             self.parrent.backpropegate(reward + self.reward, targetNode)  # FIXME: Do we want to return this? Don't see why, but I'm partialy blind.
